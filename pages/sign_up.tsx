65ecf5b63fe8b6f3ebe1e5aa7f0868c2869c83cf
tian <istiancz@gmail.com>
2021-02-17 23:22:37 +0800

    show errors

--- pages/sign_up.tsx
+++ pages/sign_up.tsx
@@ -1,6 +1,6 @@
 import {NextPage} from 'next'
 import React, {useCallback, useState} from 'react'
-import axios from 'axios'
+import axios, {AxiosResponse} from 'axios'
 
 const SignUp: NextPage = () => {
   const [data, setData] = useState<Data>({
@@ -8,9 +8,24 @@ const SignUp: NextPage = () => {
     password: '',
     passwordConfirmation: ''
   })
+  const [errors, setErrors] = useState({
+    username: [],
+    password: [],
+    passwordConfirmation: []
+
+  })
   const onSubmit = useCallback(async (e) => {
     e.preventDefault()
-    await axios.post(`api/v1/users`, data)
+    try {
+      await axios.post(`api/v1/users`, data)
+    } catch (e) {
+      if (e.response) {
+        const response: AxiosResponse = e.response
+        if (response.status === 422) {
+          setErrors({...errors, ...response.data})
+        }
+      }
+    }
   }, [data])
 
   return (
@@ -22,6 +37,7 @@ const SignUp: NextPage = () => {
             用户名
             <input type="text" value={data.username}
                    onChange={event => setData({...data, username: event.target.value})}/>
+            {errors.username?.length > 0 && <span>{errors.username.join(',')}</span>}
           </label>
         </div>
         <div>
@@ -29,6 +45,7 @@ const SignUp: NextPage = () => {
             密码
             <input type="password" value={data.password}
                    onChange={event => setData({...data, password: event.target.value})}/>
+            {errors.password?.length > 0 && <span>{errors.password.join(',')}</span>}
           </label>
         </div>
         <div>
@@ -36,6 +53,7 @@ const SignUp: NextPage = () => {
             确认密码
             <input type="password" value={data.passwordConfirmation}
                    onChange={event => setData({...data, passwordConfirmation: event.target.value})}/>
+            {errors.passwordConfirmation?.length > 0 && <span>{errors.passwordConfirmation.join(',')}</span>}
           </label>
         </div>
         <button type={'submit'}>注册</button>
