f30b1f15c647fcb27092a0bc2ae93178415077d6
tian <istiancz@gmail.com>
2021-02-19 21:20:57 +0800

    move validate() to entity

--- pages/api/v1/users.tsx
+++ pages/api/v1/users.tsx
@@ -6,57 +6,57 @@ import md5 from 'md5'
 
 const Users: NextApiHandler = async (req, res) => {
   const {username, password, passwordConfirmation} = req.body
-  const errors = {
-    username: [] as string[],
-    password: [] as string[],
-    passwordConfirmation: [] as string[],
-  }
-  const trimName = username.trim()
   const connection = await getDatabaseConnection()
-  const duplicated = await connection.manager.findOne(User, {username})
-  if (duplicated){
-    errors.username.push('username has been token,please try to use another name')
-  }
-  if (trimName === '') {
-    errors.username.push(`username can't be empty`)
-  }
-  if (!/[a-zA-Z0-9]/.test(trimName)) {
-    errors.username.push('invalid username')
-  }
-  if (trimName.length > 42) {
-    errors.username.push('too long')
-  }
-  if (trimName.length < 2) {
-    errors.username.push('too short')
-  }
-  if (password === '') {
-    errors.password.push(`password can't be empty`)
-  }
-  if (password.length < 6) {
-    errors.password.push(`password is too short (minimum is 6 characters)`)
-  }
-  if (password !== passwordConfirmation) {
-    errors.passwordConfirmation.push('Passwords do not match')
-  }
-  const hasErrors = Object.values(errors).find(value => value.length > 0)
-  res.setHeader('Content-Type', 'application/json;charset=utf-8')
-  if (hasErrors) {
-    res.statusCode = 422
-    res.write(JSON.stringify(errors))
-  } else {
-    const user = new User()
-    user.username = trimName
-    user.passwordDigest = md5(password)
-    if (!duplicated) {
-      await connection.manager.save(user)
-      res.statusCode = 200
-      res.write(JSON.stringify(user))
-    } else {
-      res.statusCode = 500
-      res.write('')
-    }
+  const user = new User()
+  user.username = username.trim()
+  user.passwordDigest = md5(password)
+  user.password = password
+  user.passwordConfirmation = passwordConfirmation
 
+  await user.validate()
+  res.setHeader('Content-Type', 'application/json;charset=utf-8')
+  if (user.hasErrors()){
+    res.statusCode = 422
+    res.write(JSON.stringify(user.errors))
+  }else {
+    await connection.manager.save(user)
+    res.statusCode = 200
+    res.write(JSON.stringify(user))
   }
   res.end()
+  // const errors = {
+  //   username: [] as string[],
+  //   password: [] as string[],
+  //   passwordConfirmation: [] as string[],
+  // }
+  // const trimName = username.trim()
+  // const connection = await getDatabaseConnection()
+  // const duplicated = await connection.manager.findOne(User, {username})
+  // if (duplicated){
+  //   errors.username.push('username has been token,please try to use another name')
+  // }
+  // if (trimName === '') {
+  //   errors.username.push(`username can't be empty`)
+  // }
+  // if (!/[a-zA-Z0-9]/.test(trimName)) {
+  //   errors.username.push('invalid username')
+  // }
+  // if (trimName.length > 42) {
+  //   errors.username.push('too long')
+  // }
+  // if (trimName.length < 2) {
+  //   errors.username.push('too short')
+  // }
+  // if (password === '') {
+  //   errors.password.push(`password can't be empty`)
+  // }
+  // if (password.length < 6) {
+  //   errors.password.push(`password is too short (minimum is 6 characters)`)
+  // }
+  // if (password !== passwordConfirmation) {
+  //   errors.passwordConfirmation.push('Passwords do not match')
+  // }
+  // const hasErrors = Object.values(errors).find(value => value.length > 0)
+
 }
 export default Users
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,6 +1,7 @@
 import {Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn} from "typeorm"
 import {Post} from './Post'
 import {Comment} from './Comment'
+import {getDatabaseConnection} from '../../lib/getDatabaseConnection'
 
 @Entity('users')
 export class User {
@@ -18,4 +19,46 @@ export class User {
   posts: Post[]
   @OneToMany(type => Comment, comment => comment.user)
   comments: Comment[]
+  errors = {
+    username: [] as string[],
+    password: [] as string[],
+    passwordConfirmation: [] as string[],
+  }
+  password: string
+  passwordConfirmation: string
+
+  async validate() {
+    const trimName = this.username.trim()
+    const connection = await getDatabaseConnection()
+    const duplicated = await connection.manager.findOne(User, {username: this.username})
+    if (!!duplicated) {
+      this.errors.username.push('username has been token,please try to use another name')
+    }
+    if (trimName === '') {
+      this.errors.username.push(`username can't be empty`)
+    }
+    if (!/[a-zA-Z0-9]/.test(trimName)) {
+      this.errors.username.push('invalid username')
+    }
+    if (trimName.length > 42) {
+      this.errors.username.push('too long')
+    }
+    if (trimName.length < 2) {
+      this.errors.username.push('too short')
+    }
+    if (this.password === '') {
+      this.errors.password.push(`password can't be empty`)
+    }
+    if (this.password.length < 6) {
+      this.errors.password.push(`password is too short (minimum is 6 characters)`)
+    }
+    if (this.password !== this.passwordConfirmation) {
+      this.errors.passwordConfirmation.push('Passwords do not match')
+    }
+    return this.errors
+  }
+
+  hasErrors() {
+    return !!Object.values(this.errors).find(value => value.length > 0)
+  }
 }
