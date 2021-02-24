50297663c9288a24b9fcc6a2f625d833a0b8d680
tian <istiancz@gmail.com>
2021-02-24 21:48:49 +0800

    fix a ts-ignore

--- lib/withSession.tsx
+++ lib/withSession.tsx
@@ -1,10 +1,10 @@
-import {withIronSession} from 'next-iron-session';
-import {NextApiHandler} from 'next';
+import {withIronSession} from 'next-iron-session'
+import {GetServerSideProps, NextApiHandler} from 'next'
 
-export function withSession(handler: NextApiHandler) {
+export function withSession(handler: NextApiHandler | GetServerSideProps) {
   return withIronSession(handler, {
     password: process.env.SECRET_COOKIE_PASSWORD,
     cookieName: 'blog',
     cookieOptions: {secure: false}
-  });
+  })
 }
--- pages/sign_in.tsx
+++ pages/sign_in.tsx
@@ -1,4 +1,4 @@
-import {GetServerSideProps, NextPage} from 'next'
+import {GetServerSideProps, GetServerSidePropsContext, NextPage} from 'next'
 import React, {useCallback, useState} from 'react'
 import axios, {AxiosResponse} from 'axios'
 import {withSession} from '../lib/withSession'
@@ -66,8 +66,7 @@ interface Data {
 export default SignIn
 
 // todo
-// @ts-ignore
-export const getServerSideProps: GetServerSideProps = withSession(async (context) => {
+export const getServerSideProps: GetServerSideProps = withSession(async (context: GetServerSidePropsContext) => {
   // @ts-ignore
   const user = context.req.session.get('currentUser')
   return {
