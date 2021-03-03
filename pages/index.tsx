e0cf150ca83cdf93c05c068e2b7af82317abe921
tian <istiancz@gmail.com>
2021-03-03 15:36:44 +0800

    pagination 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,13 +7,27 @@ import qs from 'query-string'
 
 type Props = {
   posts: Post[]
-  total:number
+  total: number
+  page: number
+  size: number
 }
 const Posts: NextPage<Props> = (props) => {
-  const {posts,total} = props
+  const {posts, total, page, size} = props
+  const totalPage = Math.ceil(total / size)
+
+  const validPage = (page: number) => {
+    if (page <= 0) {
+      return 1
+    }
+    if (page > totalPage) {
+      return totalPage
+    }
+    return page
+  }
+
   return (
     <div>
-      <h1>文章列表 total : {total}</h1>
+      <h1>文章列表</h1>
       {posts.map(posts =>
         <div key={posts.id}>
           <Link href={`/posts/${posts.id}`}>
@@ -21,6 +35,18 @@ const Posts: NextPage<Props> = (props) => {
           </Link>
         </div>
       )}
+      <div>
+        <Link href={`/posts?page=${validPage(page - 1)}`}>
+          <a>previous</a>
+        </Link>
+        |
+        <Link href={`/posts?page=${validPage(page + 1)}`}>
+          <a>next</a>
+        </Link>
+        |
+        {page}/{totalPage} , total {total} items
+      </div>
+
     </div>
   )
 }
@@ -30,16 +56,16 @@ export default Posts
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const connection = await getDatabaseConnection()
   const index = context.req.url.indexOf('?')
-  const query = qs.parse(context.req.url.substr(index))
-  const page = parseInt(query.page.toString() || '1')
-  console.log(page)
+  const query = qs.parse(context.req.url.substr(index > 0 ? index : 0))
+  const page = parseInt(query.page?.toString() || '1')
   const size = 3
-  const [posts,total] = await connection.manager.findAndCount(Post, {skip: (page - 1) * size,take:size})
+  const [posts, total] = await connection.manager.findAndCount(Post, {skip: (page - 1) * size, take: size})
 
   return {
     props: {
       posts: JSON.parse(JSON.stringify(posts)),
-      total
+      total,
+      page, size
     }
   }
 }
