d0c65c3e2e23f53e64ee0d08795fdf298630e9be
tian <istiancz@gmail.com>
2021-03-04 15:31:49 +0800

    add comment

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,18 +1,44 @@
-import React from 'react'
+import React, {FormEventHandler, useState} from 'react'
 import {GetServerSideProps, NextPage} from 'next'
 import {getDatabaseConnection} from '../../lib/getDatabaseConnection'
 import {Post} from '../../src/entity/Post'
+import {comment} from 'postcss'
+import axios, {AxiosResponse} from 'axios'
+import {useForm} from '../../hooks/useForm'
+import qs from 'query-string'
 
 type Props = {
   post: Post
 }
 const postsShow: NextPage<Props> = (props) => {
   const {post} = props
+
+  const {form} = useForm({comment: ''},
+    [{label: '', inputType: 'textArea', key: 'comment'}],
+    <button type={'submit'}>comment</button>,
+    {
+      request: (data) => axios.post(`api/v1/sessions`, data), success: () => {
+        window.alert('success!')
+      }
+    })
+
   return (
     <div>
       <h1>{post.title}</h1>
       <article dangerouslySetInnerHTML={{__html: post.content}}>
       </article>
+      <footer>
+        <h1>comments</h1>
+        {form}
+        <hr />
+        {post.comments?.map(comment => <div>
+          <h2>{comment.user}</h2>
+          <div>
+            {comment.content}
+          </div>
+          <span>createdAt {comment.createdAt}</span>
+        </div>)}
+      </footer>
     </div>
   )
 }
